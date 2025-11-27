358f4f4addd8d2598fd2bab09c2b8fe9fe9e1321
trgiabao830 <trgiabao830@gmail.com>
2025-11-27 08:09:16 +0700

    Initial commit

---
+++ backend/src/main/java/com/tgb/cp_dns/CpDnsApplication.java
@@ -0,0 +1,13 @@
+package com.tgb.cp_dns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpDnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpDnsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tgb/cp_dns/CpDnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgb.cp_dns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpDnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

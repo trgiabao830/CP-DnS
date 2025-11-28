72a9d3e298a17ae61458daea76c0892c7aaa68b5
trgiabao830 <trgiabao830@gmail.com>
2025-11-28 12:50:06 +0700

    feat: add initial entities and enums

--- backend/src/main/java/com/tgb/cp_dns/entity/log/HomestayRoomLog.java
+++ backend/src/main/java/com/tgb/cp_dns/entity/log/HomestayRoomLog.java
@@ -1,5 +1,21 @@
 package com.tgb.cp_dns.entity.log;
 
-public class HomestayRoomLog {
-    
+import com.tgb.cp_dns.entity.homestay.Room;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Table(name = "homestay_room_logs")
+public class HomestayRoomLog extends BaseLog {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id", nullable = false)
+    private Room room;
+
+    @Column(name = "action_type")
+    private String actionType;
 }
--- backend/src/main/java/com/tgb/cp_dns/entity/log/RestaurantTableLog.java
+++ backend/src/main/java/com/tgb/cp_dns/entity/log/RestaurantTableLog.java
@@ -1,5 +1,21 @@
 package com.tgb.cp_dns.entity.log;
 
-public class RestaurantTableLog {
-    
+import com.tgb.cp_dns.entity.restaurant.RestaurantTable;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Table(name = "restaurant_table_logs")
+public class RestaurantTableLog extends BaseLog {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "table_id", nullable = false)
+    private RestaurantTable table;
+
+    @Column(name = "action_type")
+    private String actionType;
 }
